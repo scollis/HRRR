73a67d281f97cec973782179d4dbf8b61e9b2f43
mattjohnson <mjohnson541@gmail.com>
2014-06-06 14:56:07 -0500

    initial construction

--- functions/HRRR_inventory.py
+++ functions/HRRR_inventory.py
@@ -6,7 +6,11 @@ Created on Fri Jun  6 09:35:18 2014
 """
 
 def HRRR_inventory(in_dir):
+    """
+    finds the number of HRRR files in a given directory that correspond to each date
+    returns that in a list along with a list of the corresponding dates.  
+    """
     x = gather_hrrr_files(in_dir)
     x = x[0]
     y = [len(x[i]) for i in range(len(x))]
-    return y
+    return [y, x[1]]
